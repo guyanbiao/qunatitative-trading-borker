4858a62cc03430bd46ae72257d0fe0ffb960c5f1
groot <groot@crypto.com>
2021-05-23 17:21:46 +0800

    add nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :order_executions
   resources :settings, only: [:index, :create]
+  root to: 'order_executions#index'
 end
