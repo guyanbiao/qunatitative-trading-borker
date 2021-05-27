a0f27b8cb4e8db99e3e9d25894bd13c5c6d75105
groot <groot@crypto.com>
2021-05-27 23:12:19 +0800

    fix login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_order_percentage, numericality: { greater_than: 0, less_than_or_equal_to: 1 }, allow_nil: true
-  validates_uniqueness_of :webhook_token
-  validates_length_of :webhook_token, minimum: 10
+  validates_uniqueness_of :webhook_token, allow_blank: true
+  validates_length_of :webhook_token, minimum: 10, allow_blank: true
 end
