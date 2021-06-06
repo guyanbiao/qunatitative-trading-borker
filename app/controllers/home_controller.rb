6545140927e1c7e030a5a53233c10ed91095176f
groot <groot@crypto.com>
2021-06-06 14:42:06 +0800

    fix bug for new user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   def index
     @currency = current_user.order_executions.last&.currency || Setting.support_currencies.first
     @exchange = current_user.get_exchange(@currency)
-    @position =  @exchange.current_position
-    @orders = @exchange.closed_orders
+    if @exchange.credentials_set?
+      @position =  @exchange.current_position
+      @orders = @exchange.closed_orders
+    else
+      @position = nil
+      @orders = []
+    end
   end
 end
