6c5cc7ad8f5b0e0d1a76f60216eb905112a36d32
groot <groot@crypto.com>
2021-06-30 21:25:25 +0800

    add log

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,6 +4,10 @@ class WebhooksController < ApplicationController
 
   def alert
     Rails.logger.info("[alert_log] ip=#{request.remote_ip} params=#{params.inspect}")
+    Rails.logger.info("[alert_log headers] remote_ip=#{request.headers['X-Forwarded-For']}")
+    Rails.logger.info("[alert_log full headers] headers=#{request.headers.to_h}")
+
+
     begin
       WebhookHandlingService.new(alert_params, request).execute
       render inline: "success"
