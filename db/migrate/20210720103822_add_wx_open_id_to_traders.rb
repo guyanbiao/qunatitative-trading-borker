6655630d00c4df06fca8d5ae945a5f9c7253aa0d
groot <groot@crypto.com>
2021-07-20 19:03:49 +0800

    add uniq check

--- db/migrate/20210720103822_add_wx_open_id_to_traders.rb
+++ db/migrate/20210720103822_add_wx_open_id_to_traders.rb
@@ -1,5 +1,5 @@
 class AddWxOpenIdToTraders < ActiveRecord::Migration[6.1]
   def change
-    add_column :traders, :wx_open_id, :string, index: true
+    add_column :traders, :wx_open_id, :string, index: {unique: true}
   end
 end
